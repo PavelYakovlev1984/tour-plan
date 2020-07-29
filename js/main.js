5755b9fd22f06bdde0bebd86c2431b9e397ebf4f
PavelYakovlev1984 <55947748+PavelYakovlev1984@users.noreply.github.com>
2020-07-30 02:30:33 +0500

    create: media rules for hotel section

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', { 
   // Optional parameters
   
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
+  /* var mySwiper = new Swiper ( '.swiper-container' , { keyboard : { */
+    keyboard: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
 
+
+
+
+  loop: true,
+  enabled: true,
+  
+  pageUpDown: true,
+
+  // Navigation arrows
+ 
+
  /* effect: "cube", */
-})
+/* })
 $(document).ready(function(){
 $( "input" ).keyup(function(){
 
-});
+}); */
 
 
 
 });
+
+/* var mySwiper = new Swiper('.swiper-container', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  }, */
+/* }); */
