c596b5951850a4235ca3d6f35e1a3f3dafa08218
PavelYakovlev1984 <55947748+PavelYakovlev1984@users.noreply.github.com>
2020-08-06 22:36:16 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.hotel-slider', { 
+$(document).ready(function(){
+  var hotelSlider = new Swiper('.hotel-slider', { 
  
     keyboard: {
     nextEl: '.hotel-slider__button--next',
@@ -30,5 +31,26 @@ var menuButton = document.querySelector(".menu-button")
 menuButton.addEventListener("click", function(){
   console.log("Клик по кнопке меню")
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
-})
+});
 
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal)
+
+function openModal(){
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+function closeModal(event){
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
+
+
+});
